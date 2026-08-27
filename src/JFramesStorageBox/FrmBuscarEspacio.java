69b7100f4b20f14d022f4cc4e3470fa09f08881a
WesleyRM13 <wruizm@est.utn.ac.cr>
2026-08-27 11:52:50 -0600

    Actualización de las propiedades del form

--- src/JFramesStorageBox/FrmBuscarEspacio.java
+++ src/JFramesStorageBox/FrmBuscarEspacio.java
@@ -67,7 +67,7 @@ public class FrmBuscarEspacio extends javax.swing.JFrame {
         tblEspacios = new javax.swing.JTable();
         btnCancelar = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel1.setText("Busqueda");
